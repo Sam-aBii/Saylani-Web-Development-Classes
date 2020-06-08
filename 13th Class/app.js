e6f060333f3bfac9469d12f9c3211203fc1d4144
Rana Abid <62594563+Sam-aBii@users.noreply.github.com>
2020-06-08 19:16:09 +0500

    Update app.js

--- 13th Class/app.js
+++ 13th Class/app.js
@@ -2,43 +2,43 @@
 
     //   Table from  1  to  100 
 
-//  for(var a = 1; a <= 100; a = a+10){
-//      for(var b = a; b <= a + 9 ; b++){
-//          document.write(b + " ");
-//      }
-//      document.write("<br>")
-//  }
+ for(var a = 1; a <= 100; a = a+10){
+     for(var b = a; b <= a + 9 ; b++){
+         document.write(b + " ");
+     }
+     document.write("<br>")
+ }
 
 
  
 
-// var cities = ["Karachi","Lahore","Islamabd","Hyderabad"];
+var cities = ["Karachi","Lahore","Islamabd","Hyderabad"];
 
-// var myCity = prompt("Enter City")
-// alert(myCity.toUpperCase());
-// alert(myCity.toLowerCase());
+var myCity = prompt("Enter City")
+alert(myCity.toUpperCase());
+alert(myCity.toLowerCase());
 
-// for(var i=0; i< cities.length; i++){
-//     if(cities[i] === myCity){
-//         alert("City Found");
-//     }else{
-//         alert("City Nost Found")
-//     }
-// }
+for(var i=0; i< cities.length; i++){
+    if(cities[i] === myCity){
+        alert("City Found");
+    }else{
+        alert("City Nost Found")
+    }
+}
 
 
    
 
-// var month = prompt("Enter a month");
+var month = prompt("Enter a month");
     
-// alert(month);
-// var monthLength = month.length;
-// alert(monthLength);
+alert(month);
+var monthLength = month.length;
+alert(monthLength);
 
-// if(monthLength > 3){
-//     alert("Correct input");
-//     alert(month.slice(0,3));
-// } else {
-//     alert("Wrong input");
-//     alert(month);
-// } 
+if(monthLength > 3){
+    alert("Correct input");
+    alert(month.slice(0,3));
+} else {
+    alert("Wrong input");
+    alert(month);
+} 
