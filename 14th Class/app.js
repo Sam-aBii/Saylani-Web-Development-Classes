499979872ed54234e169b10c7d2431ad9a1063e6
Sam-aBii <samabii786@gmail.com>
2020-06-21 23:29:47 +0500

    updated app.js

--- 14th Class/app.js
+++ 14th Class/app.js
@@ -1,84 +1,87 @@
-// function agecalc(){
-// var dob = new Date(prompt("Enter your date of bith","Jan 1, 1970"))
-// var dobmili = dob.getTime();
-// var today = new Date();
-// var todaymili = today.getTime()
-// var diff = todaymili - dobmili;
-// var accuage = Math.floor(diff/(1000*60*60*24*30*12))
-// document.write(accuage);
-// }
+function agecalc(){
+var dob = new Date(prompt("Enter your date of bith","Jan 1, 1970"))
 
-// agecalc();
-// var h = 15;
-// function add(a,b,c){
-//     alert(h);
-//     var z = a+b+c
-//     return z;
-// }
+var dobmili = dob.getTime();
+var today = new Date();
+var todaymili = today.getTime()
+var diff = todaymili - dobmili;
+var accuage = Math.floor(diff/(1000*60*60*24*30*12))
+document.write(accuage);
+}
 
-// var g = add(5,8,11);
-// alert(g);
+agecalc();
+var h = 15;
+function add(a,b,c){
+    alert(h);
+    var z = a+b+c
+    return z;
+}
 
-
-// // Parameters
-// function first(b,a){
-//    return b + a
-// }
+var g = add(5,8,11);
+alert(g);
 
 
-// // arguments 
-// console.log(first(2,3)) // 5
+// Parameters
+function first(b,a){
+   return b + a
+}
 
 
-// var a = "ghous";
-
-// function foo(){
-//     a =  "Ahmed"
-// }
-
-// console.log(a);
+// arguments 
+console.log(first(2,3)) // 5
 
 
-// function calc(num1,opr,num2){
-//     if(opr === "+"){
-//         return num1 + num2
-//     }
-//     else if(opr === "-"){
-//         return num1 - num2
-//     } 
-//     else if(opr === "*"){
-//         return num1 * num2
-//     }else{
-//         return "Incorrect Operator!"
-//     }
-// }
-// var result = calc(5,"-",5);
-// var result1 = calc(4,"*",6);
-// var result2 = calc(4,"$",9)
-// console.log(result)
-// console.log(result1)
-// console.log(result2)
+var a = "ghous";
+
+function foo(){
+    a =  "Ahmed"
+}
+
+console.log(a);
 
 
 
-// function foo(){
+function calc(num1,opr,num2){
+    if(opr === "+"){
+        return num1 + num2
+    }
+    else if(opr === "-"){
+        return num1 - num2
+    } 
+    else if(opr === "*"){
+        return num1 * num2
+    }else{
+        return "Incorrect Operator!"
+    }
+}
 
-// }
+var result = calc(5,"-",5);
+var result1 = calc(4,"*",6);
+var result2 = calc(4,"$",9)
+console.log(result)
+console.log(result1)
+console.log(result2)
 
-// console.log(foo()) // undefined
 
-// var name = "Ghous"
 
-// switch (name) {
-//     case "Ghous":
-//         alert("Hello Ghous")
-//         break;
-//     case "Basit":
-//         alert("Hello Basit")
-//         break;
-//     default:
-//         alert("hello")
-// }
+function foo(){
+
+}
+
+console.log(foo()) // undefined
+
+var name = "Ghous"
+
+switch (name) {
+    case "Ghous":
+        alert("Hello Ghous")
+        break;
+    case "Basit":
+        alert("Hello Basit")
+        break;
+    default:
+        alert("hello")
+}
 
 
 
